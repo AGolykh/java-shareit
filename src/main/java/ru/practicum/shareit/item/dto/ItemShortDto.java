887dc39f1edf5535f9eda60556c18d0562e88b5f
a.golykh <a.golykh@yandex.ru>
2023-05-27 13:27:02 +0300

    feat: BookingControllerTest

--- src/main/java/ru/practicum/shareit/item/dto/ItemShortDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemShortDto.java
@@ -1,10 +1,12 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ItemShortDto {
 
     private Long id;
@@ -12,16 +14,4 @@ public class ItemShortDto {
     private String description;
     private Boolean available;
     private Long requestId;
-
-    public ItemShortDto(String name, String description, Boolean available, Long requestId) {
-        this.name = name;
-        this.description = description;
-        this.available = available;
-        this.requestId = requestId;
-    }
-
-    public ItemShortDto(Long id, String name, String description, Boolean available, Long requestId) {
-        this(name, description, available, requestId);
-        this.id = id;
-    }
 }
